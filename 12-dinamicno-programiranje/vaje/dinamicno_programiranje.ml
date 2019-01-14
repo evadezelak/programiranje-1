4cd042656a4ae470906dedfeab405faa1557c682
Eva Deželak <eva.dezelak@gmail.com>
2019-01-14 11:55:44 +0100

    vaje dinamicno programirajnje

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -21,6 +21,36 @@ let test_matrix =
      [| 2 ; 4 ; 5 |];
      [| 7 ; 0 ; 1 |] |]
 
+let memoiziraj_rec odviti_f =
+  let rezultati = Hashtbl.create 512 in
+  let rec mem_f x =
+    if Hashtbl.mem rezultati x then
+      Hashtbl.find rezultati x
+    else
+      let y = odviti_f mem_f x in
+      Hashtbl.add rezultati x y;
+      y
+  in
+  mem_f
+
+let max_cheese cheese_matrix = 
+  let max_r = Array.length cheese_matrix in
+  let max_c = Array.length cheese_matrix.(0) in (*vzamemo prvo vrstico in pogledamo koliko elementov ima -> če je senam prazen, bo to fejlalo*)
+
+  let rec max_cheese' recursive_max_cheese' (r, c) = (*row in column*)
+    if r >= max_r || c >= max_c then
+      0
+    else 
+      let right = recursive_max_cheese' (r, c+1) in
+      let down = recursive_max_cheese' (r+1, c) in
+      let our_cheese = cheese_matrix.(r).(c) in
+      our_cheese + max right down
+  in
+  let memoised_max_cheese = memoiziraj_rec max_cheese' in
+  memoised_max_cheese (0,0)
+
+  
+
 (*----------------------------------------------------------------------------*]
  Rešujemo problem sestavljanja alternirajoče obarvanih stolpov. Imamo štiri
  različne tipe gradnikov, dva modra in dva rdeča. Modri gradniki so višin 2 in
